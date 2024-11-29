040b5e17222faee183f42c0b21e6fd279339b9ec
JhonOsorio29 <jhon.osorio.7e5@itb.cat>
2024-11-29 23:39:17 +0100

    Revert "#4"
    This reverts commit c27b315eb481308ec39feef98d68471669a46085.

--- ExerciseFour.cs
+++ ExerciseFour.cs
@@ -2,46 +2,37 @@ using System;
 
 class Program
 {
-    public const MsgLargeRect = "Introdueix l'amplada del rectangle:";
-    public const MsgAltRect = "Introdueix l'altura del rectangle:";
-    public const MsgAreaResult = "L'àrea del rectangle és: ";
-    public const MsgRadiCirc = "Introdueix el radi del cercle:";
-    public const MsgCircunResult = "La circumferència del cercle és: ";
-    public const MsgResultBig = "L'àrea és més gran de 50";
-    public const MsgResultMiddle = "L'àrea és entre 20 i 50";
-    public const MsgResultSmall = "L'àrea és menor o igual a 20";
-
     static void Main(string[] args)
     {
         // Sol·licita l'entrada de l'usuari per calcular l'àrea d'un rectangle
-        Console.WriteLine(MsglargeRect);
+        Console.WriteLine("Introdueix l'amplada del rectangle:");
         double width = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine(MsgAltRect);
+        Console.WriteLine("Introdueix l'altura del rectangle:");
         double height = Convert.ToDouble(Console.ReadLine());
 
         // Calcula l'àrea
         double area = width * height;
-        Console.WriteLine(MsgAreaResult + area);
+        Console.WriteLine("L'àrea del rectangle és: " + area);
 
         // Sol·licita l'entrada de l'usuari per calcular la circumferència d'un cercle
-        Console.WriteLine(MsgRadiCirc);
+        Console.WriteLine("Introdueix el radi del cercle:");
         double radius = Convert.ToDouble(Console.ReadLine());
         double circumference = 2 * Math.PI * radius;
 
-        Console.WriteLine(MsgCircunResult + circumference);
+        Console.WriteLine("La circumferència del cercle és: " + circumference);
 
         // Imprimeix un missatge basat en el valor de l'àrea
         if (area > 50)
         {
-            Console.WriteLine(MsgResultBig);
+            Console.WriteLine("L'àrea és més gran de 50");
         }
         else if (area > 20)
         {
-            Console.WriteLine(MsgResultMiddle);
+            Console.WriteLine("L'àrea és entre 20 i 50");
         }
         else
         {
-            Console.WriteLine(MsgResultSmall);
+            Console.WriteLine("L'àrea és menor o igual a 20");
         }
     }
 }
